525b5a5e82e686e5f904ab3f0d19c6e926b4d3ab
Chunxuan Shao <c.shao@dkfz-heidelberg.de>
2022-02-28 15:30:43 +0100

    feat: add data sections

--- R/data.R
+++ R/data.R
@@ -4,3 +4,31 @@
 #'
 #' @format A data.table with three columns: "screen_entrez", "screen_symbol" and "cell_line".
 "brca_screen"
+
+#' Mutations to be use in the comp_slp
+#'
+#' Mutations and related TCGA ids.
+#'
+#' @format A data.table.
+"comp_mut"
+
+#' Mutations to be use in the corr_slp
+#'
+#' Mutations and related TCGA ids.
+#'
+#' @format A data.table.
+"corr_mut"
+
+#' Expression data to be used in comp_slp
+#'
+#' Expresion matrix, genes by samples.
+#'
+#' @format A matrix.
+"example_expr"
+
+#' Expression data to be used in corr_slp
+#'
+#' Z score matrix, genes by samples.
+#'
+#' @format A matrix.
+"example_z"
